10876100ef58468b8e59020fc5c62d34e94a6af4
Riesi <Riesi@users.noreply.github.com>
2023-10-10 12:42:32 -0700

    Update build_vs.bat

--- build/workspaces/build_vs.bat
+++ build/workspaces/build_vs.bat
@@ -3,10 +3,4 @@ cd /d %~dp0
 
 call update-workspaces.bat
 
-Rem winget install vswhere
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`vswhere.exe -latest -find "**/Bin/MSBuild.exe"`) DO (
-SET msbpath=%%F
-)
-Rem "%msbpath%" vs2017/pyrogenesis.sln /p:Configuration=Release /p:PlatformTarget=win32
-MSBuild.exe build\workspaces\vs2017\pyrogenesis.sln /p:Configuration=Release /p:PlatformTarget=win32
+MSBuild.exe vs2017\pyrogenesis.sln /p:Configuration=Release /p:PlatformTarget=win32
